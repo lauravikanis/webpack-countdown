90d8c6a64d47a53f220368e0e97db23b4f87dc47
lauravikanis <lauravikanis@yahoo.de>
2020-10-20 15:17:06 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
 import "./app.css";
+import { createElement } from "./element";
 
 export function App() {
-  const app = document.createElement("div");
-  app.className = "app";
-  app.innerHTML = "<b>Awesome</b> Webpack Template!";
+  const app = createElement("div", {
+    className: "app",
+    children: [
+      createElement("div", {
+        className: "time",
+        innerText: createCountdownTimer(),
+      }),
+      createElement("button", {
+        classname: "startTimeButton",
+        innerText: "Go!",
+        onclick: () => {
+          createCountdownTimer();
+        },
+      }),
+      createElement("input", {
+        className: "Input_Number",
+      }),
+    ],
+  });
+  function createCountdownTimer() {
+    let sec = document.querySelector(".Input_Number");
+    let intervalId = setInterval(function () {
+      document.querySelector(".time").innerText = sec;
+      sec--;
+      if (sec === 0) {
+        clearInterval(intervalId);
+      }
+    }, 1000);
+  }
   return app;
 }
+
+//   app.className = "startButton";
+//   app.innerHTML = "Start Countdown";
+
+//   // onclick function to start the countdown
+//   app.onclick = function startTimer() {
+//     let seconds = document.querySelector(".timer").textContent;
+//     let countdown = setInterval(function () {
+//       seconds--;
+//       document.querySelector(".timer").textContent = seconds;
+//       if (seconds <= 0) clearInterval(countdown);
+//     }, 1000);
+//   };
+
+//   return app;
+// }
+
+// export function Timer() {
+//   const timer = document.createElement("span");
+//   timer.className = "timer";
+//   timer.innerText = "20";
+
+//   return timer;
+// }
